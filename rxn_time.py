b8956d693cc588ec0285e80358284b7416b1feff
Courtney McBeth <cmcbeth5287@gmail.com>
2019-08-15 12:02:34 -0400

    Improved delay
    Now adds a delay between repeated presses of the same key/keys

--- rxn_time.py
+++ rxn_time.py
@@ -1,19 +1,23 @@
-filename = "the_entertainer.txt"
-addOn = 50
+filename = "flight_of_the_bumblebee.txt"
+addOn = 100
 delay = 0
 
 newName = filename.split(".")[0] + "_rxn.txt"
 
 lastTime = 0
+lastKeys = []
 with open(filename, "r") as origFile:
     with open(newName, "w") as newFile:
         line = origFile.readline()
         while not "eof" in line:
             lst = line.split(",")
-            if "off" in lst[2]:
+            if "off" in lst[2] or int(lst[1]) in lastKeys:
                 if not int(lst[0]) == lastTime:
                     delay = delay + addOn
                     lastTime = int(lst[0])
+                    lastKeys = [int(lst[1])]
+                else:
+                    lastKeys.append(int(lst[1]))
                 newFile.write(str(int(lst[0]) + delay) +
                               "," + lst[1] + "," + lst[2])
             else:
